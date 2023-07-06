8593d36058f52db9189e5650b085e9f8b77bed8e
DenialOfIntelligence <135235065+DenialOfIntelligence@users.noreply.github.com>
2023-07-06 13:25:21 +0200

    Syntax & intendation solved

--- draw.py
+++ draw.py
@@ -6,21 +6,21 @@ x2=0
 y2=0
 draw_line(0,0,0,0)
 while get_key() != "esc":
-	if get_key() = "a":
-		while get_key() != "a":
-		draw_text(0,214,"Drawing line!")
-			if get_key()== "center":
-				if x1==0:
-					print("First pos")
-					x1=get_mouse()[0]
-					y1=get_mouse()[1]
-					draw_rect(x1,y1,1,1)
-				else:
-					print("Sec pos")
-					x2=get_mouse()[0]
-					y2=get_mouse()[1]
-					draw_line(x1,y1,x2,y2)
-					x1=0
-					y1=0
-					x2=0
-					y2=0
+  if get_key() == "a":
+    while get_key() != "esc":
+      draw_text(2,214,"Drawing Line")
+      if get_key()== "center":
+        if x1==0:
+          print("First pos")
+          x1=get_mouse()[0]
+          y1=get_mouse()[1]
+          draw_rect(x1,y1,1,1)
+        else:
+          print("Sec pos")
+          x2=get_mouse()[0]
+          y2=get_mouse()[1]
+          draw_line(x1,y1,x2,y2)
+          x1=0
+          y1=0
+          x2=0
+          y2=0
