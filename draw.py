2302886cde58309f9f84c9d4d97f3980c668408c
DenialOfIntelligence <135235065+DenialOfIntelligence@users.noreply.github.com>
2023-07-06 13:07:46 +0200

    Make it run

--- draw.py
+++ draw.py
@@ -1,17 +1,23 @@
 from ti_system import *
 from ti_draw import *
-
+x1=0
+y1=0
+x2=0
+y2=0
 draw_line(0,0,0,0)
 while get_key() != "esc":
-	if get_key()== "center":
-		if not x1==0:
-			x1=get_mouse()[0]
-			y1=get_mouse()[1]
-		else:
-			x2=get_mouse()[0]
-			y2=get_mouse()[1]
-			draw_line(x1,y1,x2,y2)
-			x1=0
-			y1=0
-			x2=0
-			y2=0
+  if get_key()== "center":
+    if x1==0:
+      print("First pos")
+      x1=get_mouse()[0]
+      y1=get_mouse()[1]
+      draw_rect(x1,y1,1,1)
+    else:
+      print("Sec pos")
+      x2=get_mouse()[0]
+      y2=get_mouse()[1]
+      draw_line(x1,y1,x2,y2)
+      x1=0
+      y1=0
+      x2=0
+      y2=0
