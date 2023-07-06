b2602616c20262fa7d7a39f347ec1161a90deeed
DenialOfIntelligence <renkenjakob@gmail.com>
2023-07-06 13:53:39 +0200

    Now using classes

--- draw.py
+++ draw.py
@@ -1,5 +1,15 @@
 from ti_system import *
 from ti_draw import *
+class line:
+	def __init__(self, x1, y1, x2, y2, name):
+		self.x1=x1
+		self.y1=y1
+		self.x2=x2
+		self.y2=y2
+		self.name=name
+	def draw(self):
+		draw_line(self.x1,self.y1,self.x2,self.y2)
+lines=[]
 x1=0
 y1=0
 x2=0
@@ -19,8 +29,13 @@ while get_key() != "esc":
           print("Sec pos")
           x2=get_mouse()[0]
           y2=get_mouse()[1]
-          draw_line(x1,y1,x2,y2)
+					name = f"line'{len(lines)+1}"
+					instance=line(x1,y1,x2,y2,name)
+					lines.append(instance)
           x1=0
           y1=0
           x2=0
           y2=0
+          clear()
+			for instance in lines:
+				instance.draw()
